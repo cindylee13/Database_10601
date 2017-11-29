e86037d5e26adc77cd9c20edd77067aa03f8334f
sunny2920 <t104590454@ntut.org.tw>
2017-11-30 00:02:39 +0800

    add css.php(將要include的css檔包再一起)

--- EachCategory.php
+++ EachCategory.php
@@ -11,18 +11,7 @@ include('db.php');
 <html lang="en">
 <head>
 <title>Resale_v2 a Classified ads Category Flat Bootstrap Responsive Website Template | Home :: w3layouts</title>
-<link rel="stylesheet" href="css/bootstrap.min.css"><!-- bootstrap-CSS -->
-<link rel="stylesheet" href="css/bootstrap-select.css"><!-- bootstrap-select-CSS -->
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all" /><!-- style.css -->
-<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" /><!-- flexslider-CSS -->
-<link rel="stylesheet" href="css/font-awesome.min.css" /><!-- fontawesome-CSS -->
-<link rel="stylesheet" href="css/menu_sideslide.css" type="text/css" media="all"><!-- Navigation-CSS -->
-<!-- meta tags -->
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Resale Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<?php include("css.php"); ?>
 <!-- //meta tags -->
 <!--fonts-->
 
